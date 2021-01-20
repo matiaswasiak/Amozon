b2bf4737628eb150133b7ec864bceac00f83f4a1
matiaswasiak <matiaswasiak@gmail.com>
2021-01-19 21:04:45 -0300

    last commit

--- src/Menu.js
+++ src/Menu.js
@@ -1,21 +1,47 @@
-import React from "react";
-import Search from "./Search";
+import React, { Fragment } from "react";
 import "./Menu.css";
+import Search from "./Search";
+import PanelAdd from "./PanelAdd";
 
-function Menu(props) {
-  return (
-    <div className="container">
-      <div className="subcontainer">
-        <div className="logo">{props.title}</div>
-        <div className="search">
-          <Search />
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { newItemPanel: false };
+
+    this.add = this.add.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+  }
+
+  add() {
+    this.setState({ newItemPanel: true });
+  }
+
+  onCancel() {
+    this.setState({ newItemPanel: false });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="subcontainer">
+          <div className="logo">{this.props.title}</div>
+          <div className="search">
+            <Search onsearch={this.props.onsearch} />
+          </div>
+          <div className="actions">
+            <button onClick={this.add} className="button btn-blue">
+              + Añadir nuevo libro
+            </button>
+          </div>
         </div>
-        <div className="actions">
-          <button className="button btn-blue">+ Añadir nuevo libro</button>
-        </div>
+        {this.state.newItemPanel ? (
+          <PanelAdd onhide={this.onCancel} onadd={this.props.onadd} />
+        ) : (
+          ""
+        )}
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Menu;
